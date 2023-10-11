fb075978e04de100dd598ce17647bf09c18f9de8
liambansal <55756009+liambansal@users.noreply.github.com>
2023-10-11 03:02:14 +0100

    Fixed Missing Dependencies
    - Renamed the solution file.
    - Stopped tracking the Framework project's output directory.
    - Fixed missing file dependencies (Glad and Imgui files) by correcting file paths under the projects' resource filter.

--- Application/source/Application.cpp
+++ Application/source/Application.cpp
@@ -5,8 +5,6 @@
 
 // File's header.
 #include "Application.h"
-#include "backends/imgui_impl_glfw.h"
-#include "backends/imgui_impl_opengl3.h"
 #include "BrainComponent.h"
 #include "ColliderComponent.h"
 #include "Entity.h"
